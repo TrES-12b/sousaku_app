d69b15f3f4776caa2f06a28f02eef5fdce56d622
TrES-12b <j2104@matsue-ct.ac.jp>
2025-04-17 11:31:26 +0900

    ホーム画面用コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "home/index"
   get "worlds/index"
   get "worlds/show"
   get "characters/index"
@@ -20,6 +21,7 @@ Rails.application.routes.draw do
 end
 
 Rails.application.routes.draw do
+  get "home/index"
   get "worlds/index"
   get "worlds/show"
   resources :characters
